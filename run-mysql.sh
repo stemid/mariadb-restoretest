67b58f2a335b8b2ebb58df8c62386d9f3584b249
Stefan Midjich <swehack@gmail.com>
2017-05-19 21:57:18 +0200

    added option to use different datadir

--- run-mysql.sh
+++ run-mysql.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+test -f ./local-settings && source ./local-settings
+
+datadir="${mysql_datadir:-/var/db/mysql}"
+
 # Delete old logs that might prevent startup
-rm /var/db/mysql/aria_log_control
-rm /var/db/mysql/ib_logfile*
+rm "$datadir/aria_log_control"
+rm "$datadir/ib_logfile"*
 
 # Run mysqld without grants to allow easy login without password for root
-exec /usr/sbin/mysqld --basedir=/usr --datadir=/var/db/mysql --user=root --skip-grant-tables
+exec /usr/sbin/mysqld --basedir=/usr --datadir="$datadir" --user=root --skip-grant-tables
