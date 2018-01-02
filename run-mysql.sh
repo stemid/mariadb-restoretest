3063ef2561ea38d1756a2ecd031169a17bbf5866
Stefan Midjich <swehack@gmail.com>
2018-01-02 21:48:26 +0100

    capitalize MYSQL_DATADIR for inclusion from local-settings and remove file deletions, see comment.

--- run-mysql.sh
+++ run-mysql.sh
@@ -2,11 +2,12 @@
 
 test -f /tmp/run.conf.d/local-settings && source /tmp/run.conf.d/local-settings
 
-datadir="${mysql_datadir:-/var/db/mysql}"
+datadir="${MYSQL_DATADIR:-/var/db/mysql}"
 
 # Delete old logs that might prevent startup
-rm "$datadir/aria_log_control"
-rm "$datadir/ib_logfile"*
+# This is no longer necessary with mariadb 10 and xtrabackup >=2.2 it seems.
+#rm "$datadir/aria_log_control"
+#rm "$datadir/ib_logfile"*
 
 # Run mysqld without grants to allow easy login without password for root
 exec /usr/sbin/mysqld --basedir=/usr --datadir="$datadir" --user=root --skip-grant-tables
